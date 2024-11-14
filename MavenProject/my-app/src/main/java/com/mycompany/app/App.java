3de8827c15582dccafc8ca3464f1fa2c201e344b
mikelmujika16 <124912823+mikelmujika16@users.noreply.github.com>
2024-11-14 10:58:33 +0000

    sum

--- MavenProject/my-app/src/main/java/com/mycompany/app/App.java
+++ MavenProject/my-app/src/main/java/com/mycompany/app/App.java
@@ -6,5 +6,14 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //first number
+        int a = 10;
+        //second number
+        int b = 20;
+        //sum of two numbers
+        int c = a + b;
+        System.out.println("Sum of a and b is: " + c);
+        System.out.println("Hello World!");
+
     }
 }
